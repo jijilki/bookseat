3163060bb25d304dafce56b9636f14193e09fe1b
unknown <jijilki@gmail.com>
2020-08-30 02:04:52 -0400

    Initial Project Setup

---
+++ src/main/java/com/learning/bookseat/BookseatApplication.java
@@ -0,0 +1,13 @@
+package com.learning.bookseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookseatApplication.class, args);
+	}
+
+}
